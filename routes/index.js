1ae12f90de6bac8537ebb64ef8059f8cb9def21e
ev_massive <ev@massive.ag>
2018-12-07 17:15:57 -0300

    Sitio Completo - No Responsive

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var blog = require('../blog.json');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Rebel Streamin Solutions', notas: blog });
+  res.render('index', { title: 'Rebel Streamin Solutions', notas:blog});
 });
 
 module.exports = router;
